d49e39483c5b012897234c52a7bb90072ee6c3c2
F_Rampolla <f.rampolla98@gmail.com>
2021-02-15 21:36:13 +0100

    commitradio

---
+++ ProgettoVR-Museo/Assets/Script/MovePath.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+ 
+
+public class MovePath : MonoBehaviour
+
+{
+
+  
+    [SerializeField] private Transform _waypointRoot;
+    [SerializeField] private float _pathDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(_waypointRoot != null && _waypointRoot.childCount > 0)
+        {
+            Vector3[] pathPosition = new Vector3[_waypointRoot.childCount];
+            for (int i = 0; i < _waypointRoot.childCount; i++)
+                pathPosition[i] = _waypointRoot.GetChild(i).position;
+
+            //var tween = transform.DOPath(pathPosition, _pathDuration, PathType.Linear, PathMode.Full3D,
+            //    resolution: 10, Color.yellow).SetLoops(-1, LoopType.Yoyo);
+            GetComponent<Rigidbody>().DOPath(pathPosition, _pathDuration, PathType.Linear, PathMode.Full3D,
+               resolution: 10, Color.yellow).SetLoops(-1, LoopType.Yoyo);
+
+
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // transform.DOPause();
+        GetComponent<Rigidbody>().DOPause();
+       
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        // transform.DOPlay();
+        GetComponent<Rigidbody>().DOPlay();
+    }
+}
