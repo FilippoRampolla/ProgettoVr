e3a08525d707d8bea3e1728eef45ca5144026c17
Coviiv <virginiamencarelli@icloud.com>
2021-02-16 17:19:32 +0100

    update

---
+++ ProgettoVR-Museo/Assets/Script/Dialog_lamp.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog_lamp_finale : MonoBehaviour
+
+
+{
+
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject NPC;
+
+    public GameObject camera;
+    public GameObject fps;
+    public GameObject continueButton;
+    public GameObject start;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        NPC.GetComponent<SadIdle>().enabled = false;
+        StartCoroutine(Type());
+        start.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        fps.GetComponent<FirstPersonCharacterController>().enabled = false;
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        NPC.GetComponent<Animation>().CrossFadeQueued("Talking");
+        NPC.GetComponent<Animation>().CrossFadeQueued("Talking1");
+
+    }  
+    
+    void Update()
+    {
+       
+        if (textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+
+        }
+    }
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+
+        }
+    }
+    // Update is called once per frame
+
+    public void NextSentences()
+    {
+        continueButton.SetActive(false);
+
+        if(index< sentences.Length-1)
+
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        } else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+            start.SetActive(false);
+            fps.GetComponent<FirstPersonCharacterController>().enabled = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            NPC.GetComponent<NPCFollow>().enabled = true;
+
+        }
+
+    }
+
+    
+}
