6992af89ac1480c9200f2c141759d553ddb5d838
F_Rampolla <f.rampolla98@gmail.com>
2021-02-16 04:04:40 +0100

    DIapason

--- ProgettoVR-Museo/Assets/Script/Esperimento_diapason.cs
+++ ProgettoVR-Museo/Assets/Script/Esperimento_diapason.cs
@@ -7,18 +7,18 @@ public class Esperimento_diapason : MonoBehaviour
 
     public GameObject diapason;
     private Transform posizioneD;
-    private Transform posizioneM;
-    public GameObject martello;
-    private Transform posizioneS;
-    public GameObject sfera;
+   // private Transform posizioneM;
+   // public GameObject martello;
+   // private Transform posizioneS;
+   // public GameObject sfera;
 
     // Start is called before the first frame update
     void Start()
     {
 
         posizioneD = diapason.transform;
-        posizioneM = martello.transform;
-        posizioneS = sfera.transform;
+       // posizioneM = martello.transform;
+        //posizioneS = sfera.transform;
     }
 
 
@@ -27,8 +27,9 @@ public class Esperimento_diapason : MonoBehaviour
         if (Input.GetKeyDown("space"))
         {
             Instantiate(diapason, posizioneD.position, posizioneD.rotation);
-            Instantiate(martello, posizioneM.position, posizioneM.rotation);
-            Instantiate(sfera, posizioneS.position, posizioneS.rotation);
+            //Instantiate(martello, posizioneM.position, posizioneM.rotation);
+            //Instantiate(sfera, posizioneS.position, posizioneS.rotation);
+            Debug.Log("in");
 
         }
     }
