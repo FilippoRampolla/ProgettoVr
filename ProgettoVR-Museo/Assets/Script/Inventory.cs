f11f37eaa023e6ad036f525e9538ba7464a66e9f
F_Rampolla <f.rampolla98@gmail.com>
2021-02-16 21:40:46 +0100

    inventoryOK

---
+++ ProgettoVR-Museo/Assets/Script/Inventory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public GameObject inv;
+    public GameObject camera;
+    public GameObject fps;
+    private int flag=0;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("k") )
+        {
+            if (flag == 0) { 
+            camera.SetActive(true);
+            fps.SetActive(false);
+            inv.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            flag = 1;
+           // Debug.Log("in");
+            }
+            else
+            {
+                camera.SetActive(false);
+                fps.SetActive(true);
+                inv.SetActive(false);
+                Cursor.lockState = CursorLockMode.Locked;
+                flag = 0;
+               // Debug.Log("out");
+            }
+        }
+        
+    }
+}
