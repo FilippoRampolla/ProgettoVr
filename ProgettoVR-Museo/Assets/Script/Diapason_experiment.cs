2237591233b0ada7b80b5ae25e8e6079d3a889f1
F_Rampolla <f.rampolla98@gmail.com>
2021-02-16 04:18:48 +0100

    updatediapason

--- ProgettoVR-Museo/Assets/Script/Diapason_experiment.cs
+++ ProgettoVR-Museo/Assets/Script/Diapason_experiment.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Diapason_experiment : MonoBehaviour
 {
+
+    public GameObject fps;
+    public GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class Diapason_experiment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("i"))
+        {
+            camera.SetActive(true);
+            fps.SetActive(false);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+        }
     }
 }
---
+++ ProgettoVR-Museo/Assets/Script/MouseDrag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour
+{
+
+    public float dragSpeed = 1f;
+    Vector3 lastMousePos;
+
+    private void OnMouseDown()
+    {
+        lastMousePos = Input.mousePosition;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 delta = Input.mousePosition - lastMousePos;
+        Debug.Log(delta+ " "+ Input.mousePosition+ " "+lastMousePos);
+        Vector3 pos = transform.position;
+        pos.x += delta.x * dragSpeed;
+        pos.y += delta.y * dragSpeed;
+        
+        transform.position = pos;
+        lastMousePos = Input.mousePosition;
+    }
+
+   
+
+
+}
