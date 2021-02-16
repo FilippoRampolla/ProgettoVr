0fe93e6ecaefa7100509755f43b63ad5f4524bf8
F_Rampolla <f.rampolla98@gmail.com>
2021-02-16 03:55:13 +0100

    sistema

---
+++ ProgettoVR-Museo/Assets/Script/Esperimento_diapason.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esperimento_diapason : MonoBehaviour
+{
+
+    public GameObject diapason;
+    private Transform posizioneD;
+    private Transform posizioneM;
+    public GameObject martello;
+    private Transform posizioneS;
+    public GameObject sfera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        posizioneD = diapason.transform;
+        posizioneM = martello.transform;
+        posizioneS = sfera.transform;
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(diapason, posizioneD.position, posizioneD.rotation);
+            Instantiate(martello, posizioneM.position, posizioneM.rotation);
+            Instantiate(sfera, posizioneS.position, posizioneS.rotation);
+
+        }
+    }
+
+    
+
+
+}
---
+++ ProgettoVR-Museo/Assets/Script/UI_interactable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_interactable : MonoBehaviour
+{
+    public GameObject diapasontrigger;
+    public GameObject cavas;
+    private void OnTriggerEnter(Collider other)
+    {
+        cavas.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        cavas.SetActive(false);
+    }
+
+    void attivaCollider() 
+    {
+        diapasontrigger.GetComponent<Collider>().enabled = true;
+    }
+
+}
