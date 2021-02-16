7d5bfa782e18bf1f5500fd559843b3bfefeb4525
F_Rampolla <f.rampolla98@gmail.com>
2021-02-16 22:51:42 +0100

    light

--- ProgettoVR-Museo/Assets/Script/Target_destroy.cs
+++ ProgettoVR-Museo/Assets/Script/Target_destroy.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Target_destroy : MonoBehaviour
 {
+
+    public GameObject sprite_patate;
+    public GameObject sprite_chiodi;
+    public GameObject sprite_wire;
     public void Destory()
     {
         Destroy(gameObject);
+        sprite_patate.GetComponent<ItemSlot>().enabled = true;
+        sprite_chiodi.GetComponent<ItemSlot>().enabled = true;
+        sprite_wire.GetComponent<ItemSlot>().enabled = true;
     }
 }
