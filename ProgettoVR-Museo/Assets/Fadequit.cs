6caa455f515fa8855158586af6944a49ee6dbaae
F_Rampolla <f.rampolla98@gmail.com>
2021-02-26 13:20:02 +0100

    commit

---
+++ ProgettoVR-Museo/Assets/Fadequit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fadequit : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToload;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FadeToLevel(0);
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToload = levelIndex;
+        animator.SetTrigger("out");
+    }
+
+    public void OnFadeCOmplete()
+    {
+        SceneManager.LoadScene(levelToload);
+    }
+}
